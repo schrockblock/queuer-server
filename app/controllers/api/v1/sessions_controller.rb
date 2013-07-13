3628062185ebdbe448b59e0b543cb5857412fd76
Elliot Schrock <elliot.schrock@gmail.com>
2013-07-13 17:28:20 -0400

    added mysql cartridge to openshift

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,13 +5,17 @@ class Api::V1::SessionsController < Api::V1::ApiController
 
   def create
     @user = User.where(:username => params[:username]).first
-    if @user && @user.authenticate(params[:password])
-      @current_user = @user
-      respond_with(@user) do |format|
-        format.json { render :json => @user.as_json(:include => [:api_key]) }
+    if @user
+      if @user.authenticate(params[:password])
+        @current_user = @user
+        respond_with(@user) do |format|
+          format.json { render :json => @user.as_json(:include => [:api_key]) }
+        end
+      else
+        render(:json => { :errors => "Invalid email or password" }.to_json, :status => 401)
       end
     else
-      render(:json => { :errors => "Invalid email or password" }.to_json, :status => 401)
+      render(:json => { :errors => "Username not found, please try again" }.to_json, :status => 401)
     end
   end
 end
