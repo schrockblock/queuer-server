48fcffdbab57419bbefb976a43c7d7cd80f58999
Elliot Schrock <elliot.schrock@gmail.com>
2013-07-13 19:44:35 -0400

    added api_key and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  include TokenGenerator
-
   has_many :projects, :dependent => :destroy
   has_many :tasks, :through => :projects
 
   validates :username, :presence => true, :uniqueness => true
 
-  before_create { generate_token(:api_key) }
+  before_create set_api_key
 
-  attr_accessible :admin, :password, :username
+  attr_accessible :admin, :api_key, :password, :password_digest, :username
 
   def as_json(options={})
     included = options[:include] || {}
-    except = [:password_digest, :api_key, :admin].delete_if { |attr| included.include?(attr) }
+    except = [:password, :password_digest, :api_key, :admin].delete_if { |attr| included.include?(attr) }
 
     hash = super(:except => except)
     hash['errors'] = errors.as_json if errors.present?
 
     hash
+  end
+
+  def set_api_key
+    self.api_key = SecureRandom.urlsafe_base64
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713175549) do
+ActiveRecord::Schema.define(:version => 20130713233526) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -34,8 +34,10 @@ ActiveRecord::Schema.define(:version => 20130713175549) do
     t.string   "username"
     t.string   "password"
     t.boolean  "admin"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "api_key"
+    t.string   "password_digest"
   end
 
 end
