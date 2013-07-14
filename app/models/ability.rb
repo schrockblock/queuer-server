7dd9f5eef5f7596500275b08fea30a371f26fccc
Ellidi Jatgeirsson <ell@Ellidis-MacBook-Air.local>
2013-07-14 11:29:15 -0400

    added abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+class Ability
+  include CanCan::Ability
+
+  # NOTE: cancan is stupid about index actions
+  # You will need to explicitly prevent :index views
+  def initialize(user)
+      @user = (user ||= User.new)
+      #can :manage, :all
+      # Users
+      can :create, User
+      can :read, User 
+      can :update, User do |user|
+        user.id == @user.try(:id) || user.admin
+      end
+
+      can :index, User
+
+      # Projects and Tasks
+      can :manage, Project
+      can :manage, Task
+  end
+end
